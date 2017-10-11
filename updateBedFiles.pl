7d1a4eef7631292339a85f5d9adcad2ca44e4fb3
David A. Parry <david.parry@igmm.ed.ac.uk>
2017-10-11 11:16:27 +0100

    add options including ability to use UCSC for gene coords

--- updateBedFiles.pl
+++ updateBedFiles.pl
@@ -2,28 +2,55 @@
 use strict;
 use warnings;
 use FindBin qw($RealBin);
-print STDERR <<EOT
+use Getopt::Long;
 
-Usage: $0 [genome version] [gene_inheritance_and_diseases.txt]
+my %opts = (b => "GRCh37", g => "genes/gene_inheritance_and_diseases.txt");
+GetOptions
+(
+    \%opts,
+    'g|gene_list=s',
+    'b|build=s',
+    'u|ucsc',
+    'h|?|help',
+) or usage( "Syntax error." );
+usage() if $opts{h};
+
+sub usage{
+    my $msg = shift;
+    print STDERR "$msg\n" if $msg;
+    print STDERR <<EOT
+
+Usage: $0 [options]
+
+Options:
+
+    -b BUILD --build BUILD
+        Genome build to use. Default = GRCh37
+        Valid genome versions: hg19, hg38, GRCh37, GRCh38
+
+    -g GENELIST --gene_list GENELIST
+        File containing list of genes to retrieve coordinates for.
+        Default = "genes/gene_inheritance_and_diseases.txt"
+
+    -u    --ucsc
+        Use UCSC genome browser for retrieving gene coordinates. 
+        Useful if the Ensembl REST server is down.
 
 Example: $0 
-         $0 hg19 
-         $0 hg19 gene_file.txt
+         $0 -b hg19 
+         $0 -b hg19 -g gene_file.txt
 
-Valid genome versions: hg19 hg38 GRCh37 GRCh38
 
 EOT
-;
-
-
-my $build = shift;
-$build ||= "GRCh37";
-if ($build !~ /^(GRCh3[78]|hg(19|38))$/){
-    die "Unrecognized build '$build' given\n";
+    ;
+    exit 2 if $msg;
+    exit;
 }
 
-my $gene_list = shift;
-$gene_list ||= "genes/gene_inheritance_and_diseases.txt";
+
+if ($opts{b} !~ /^(GRCh3[78]|hg(19|38))$/){
+    die "Unrecognized build '$opts{b}' given\n";
+}
 
 my %genes = ();
 my %non_coding = ();
@@ -31,7 +58,11 @@ getGenes();
 
 getCodingExons();
 
-getGeneRegions();
+if ($opts{u}){
+    getGeneRegionsUcsc();
+}else{
+    getGeneRegions();
+}
 
 print STDERR "Done updating BED files.\n";
 
@@ -42,14 +73,14 @@ sub getGeneRegions{
         print STDERR "Retrieving genomic regions for $r genes.\n";
         my $cmd = "perl $RealBin/genomeUtils/coordinatesFromGenes.pl -j -g ". 
                   join(" ", @{$genes{$r}});
-        if ($build eq 'hg19' or $build eq 'GRCh37'){
+        if ($opts{b} eq 'hg19' or $opts{b} eq 'GRCh37'){
             $cmd .= ' -r ';
         }
-        if ($build =~ /^hg/){
+        if ($opts{b} =~ /^hg/){
             $cmd .= "| perl -wne \'chomp; print \"chr\$_\\n\"\'";
         }
         my $output = executeCommand($cmd);
-        my $bed = "bed_files/$r". "_genes_$build.bed";
+        my $bed = "bed_files/$r". "_genes_$opts{b}.bed";
         $bed =~ s/non-reportable/not_reportable/;
             #legacy reasons - we called the bed files 'not_reportable'
             #instead of 'non-reportable'
@@ -57,6 +88,31 @@ sub getGeneRegions{
         print $OUT $output;
         close $OUT;
     }
+}
+
+##################################################
+sub getGeneRegionsUcsc{
+    foreach my $r (qw /reportable non-reportable/){
+        next if not @{$genes{$r}};
+        print STDERR "Retrieving gene coordinates for $r genes from UCSC.\n";
+        my $g = join(" ", @{$genes{$r}}) ; 
+        my $cmd = "perl $RealBin/getExonsFromUcsc/getExonsFromUcsc.pl -m -k -f 500 -w -g $g";
+        if ($opts{b} eq 'GRCh38' or $opts{b} eq 'hg38'){
+            $cmd .= " -b hg38";
+        } 
+        if ($opts{b} =~ /^GRC/){
+            $cmd .= " | grep -vP '^chr\w+_' | sed s/chr// ";
+        }
+        my $output = executeCommand($cmd);
+        my $bed = "bed_files/$r". "_genes_$opts{b}.bed";
+        $bed =~ s/non-reportable/not_reportable/;
+            #legacy reasons - we called the bed files 'not_reportable'
+            #instead of 'non-reportable'
+        open (my $OUT, ">", $bed) or die "Could not open $bed for writing: $!\n";
+        print $OUT $output;
+        close $OUT;
+    }
+
 }
 
 ##################################################
@@ -69,22 +125,22 @@ sub getCodingExons{
             $g = join(" ", grep{ !$non_coding{$r}->{$_} } @{$genes{$r}});
         }
         my $cmd = "perl $RealBin/getExonsFromUcsc/getExonsFromUcsc.pl -m -k -c -g $g";
-        if ($build eq 'GRCh38' or $build eq 'hg38'){
+        if ($opts{b} eq 'GRCh38' or $opts{b} eq 'hg38'){
             $cmd .= " -b hg38";
         } 
-        $cmd .= " | sed s/chr// " if $build =~ /^GRC/;
+        $cmd .= " | sed s/chr// " if $opts{b} =~ /^GRC/;
         my $output = executeCommand($cmd);
         if ($non_coding{$r}){
             print STDERR "Appending exons of non-coding genes for $r genes.\n";
             my $ncg = join(" ", sort keys(%{$non_coding{$r}}));
             my $nc_cmd = "perl $RealBin/getExonsFromUcsc/getExonsFromUcsc.pl -m -k -g $ncg";
-            if ($build eq 'GRCh38' or $build eq 'hg38'){
+            if ($opts{b} eq 'GRCh38' or $opts{b} eq 'hg38'){
                 $nc_cmd .= " -b hg38";
             } 
             my $nc_output = executeCommand($nc_cmd);
             $output .= $nc_output;
         }
-        my $bed = "bed_files/$r". "_coding_exons_$build.bed";
+        my $bed = "bed_files/$r". "_coding_exons_$opts{b}.bed";
         $bed =~ s/non-reportable/not_reportable/;
             #legacy reasons - we called the bed files 'not_reportable'
             #instead of 'non-reportable'
@@ -96,7 +152,7 @@ sub getCodingExons{
 }
 ##################################################
 sub getGenes{
-    open (my $GENES, "<", $gene_list) or die "Could not open $gene_list: $!\n";
+    open (my $GENES, "<", $opts{g}) or die "Could not open $opts{g}: $!\n";
     my $n = 0;
     $genes{reportable} = [];
     $genes{"non-reportable"} = [];
@@ -111,7 +167,7 @@ sub getGenes{
         if (lc($r) ne 'reportable' and lc($r) ne 'non-reportable'){
             die <<EOT 
 Expected either 'reportable' or 'non-reportable' in column 3, line $n of 
-$gene_list but found '$r'. Full line was:
+$opts{g} but found '$r'. Full line was:
 $line
 Please edit this file and try again.
 EOT
@@ -122,9 +178,9 @@ EOT
             $non_coding{lc$r}->{$s}++;
         }
     }
-    die "No reportable genes identified from $gene_list!\n" if not @{$genes{reportable}};
-    print STDERR "Identified ". scalar(@{$genes{reportable}}) . " reportable genes from $gene_list.\n";
-    print STDERR "Identified ". scalar(@{$genes{"non-reportable"}}) . " non-reportable genes from $gene_list.\n";
+    die "No reportable genes identified from $opts{g}!\n" if not @{$genes{reportable}};
+    print STDERR "Identified ". scalar(@{$genes{reportable}}) . " reportable genes from $opts{g}.\n";
+    print STDERR "Identified ". scalar(@{$genes{"non-reportable"}}) . " non-reportable genes from $opts{g}.\n";
 }
 
 ##################################################
